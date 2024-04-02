8379d7e5692279e254e5d08e05fce21c65c7319a
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-04-02 05:12:17 +0200

    uplaod

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+document.body.style.overflowX = 'hidden'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
